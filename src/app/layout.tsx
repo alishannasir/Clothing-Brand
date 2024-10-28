7cdef72f049f3ce9cec3bd4a13e709e66f25849a
Alishan <140892909+alishannasir@users.noreply.github.com>
2024-10-28 00:12:24 -0700

    just started to build this mega project.

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+
+
+
+export const metadata: Metadata = {
+  title: "Clothing Brand",
+  description: "Clothing Brand",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
